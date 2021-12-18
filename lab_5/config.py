fb5b4d73e10b79823c1076db664438196576e5c3
Денис Путнов <grnbows.connect@mail.ru>
2021-12-18 10:32:01 +0300

    add 7 lab

--- lab_5/config.py
+++ lab_5/config.py
@@ -7,5 +7,5 @@ NUMBER_OF_DIGITS_AFTER_DOT = 4
 def function():
   # return lambda x: np.log(x) / x, 0.5, 5
   # return lambda x: np.sin(x) / (1 + np.cos(x)), 2, 3
-  # return lambda x: np.sin(1 + 0.5 * x), -1, 4
-  return lambda x: x / (1 + x**2), -4, 0
+  return lambda x: np.sin(1 + 0.5 * x), -1, 4
+  # return lambda x: x / (1 + x**2), -4, 0
