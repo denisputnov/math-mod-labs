21eec34b8c3acfb2b964940af0339aba203e71bc
Денис Путнов <grnbows.connect@mail.ru>
2021-12-18 11:12:53 +0300

    add hord method

---
+++ lab_7/hord_method.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+from equations import *
+import numpy as np
+
+def hord_method_with_params(x0, iterations, a, b, c, d, eps, round_to):
+  x = x0
+  x_prev = x0 + 2 * eps
+  i = 0
+
+  while np.abs(x - x_prev) >= eps and i < iterations:
+    if np.abs(derivative_phi(x)) >= 1:
+      return print("Итерационный процесс расходится")
+    x = x - fun_with_param(a, b, c, d, x) / (fun_with_param(a, b, c, d, x) - fun_with_param(a, b, c, d, x_prev)) * (x - x_prev)
+    x_prev = x
+    i += 1
+
+  print("Число итераций: ", i)
+  print("C параметрами: ", round(x, round_to))
+  plt.scatter(x, 0)
+
+
+
+def hord_method_without_params(x0, iterations, eps, round_to):
+  x = x0
+  x_prev = x0 + 2 * eps
+  i = 0
+
+  while np.abs(x - x_prev) >= eps and i < iterations:
+    if np.abs(derivative_phi(x)) >= 1:
+      return print("Итерационный процесс расходится")
+    x = x - fun(x) / (fun(x) - fun(x_prev)) * (x - x_prev)
+    x_prev = x
+    i += 1
+
+  print("Число итераций: ", i)
+  print("Без параметров: ", round(x, round_to))
+  plt.scatter(x, 0)
--- lab_7/main.py
+++ lab_7/main.py
@@ -1,3 +1,4 @@
+from hord_method import *
 from simple_iteration_method import *
 from newton_method import *
 from dichotomy_method import *
@@ -18,6 +19,20 @@ b = float(input("Параметр b: "))
 c = float(input("Параметр c: "))
 d = float(input("Параметр d: "))
 
+intervalA = float(input("Левая граница интервала: "))
+intervalB = float(input("Правая граница интервала: "))
+
+# Строим график функций для наглядности
+x = np.linspace(intervalA, intervalB, 1000)
+y1 = fun_with_param(a, b, c, d, x)
+y2 = fun(x)
+
+plt.plot(x, y1)
+plt.plot(x, y2)
+plt.grid(True)
+plt.show()
+
+
 x0 = float(input("Начальное приближение x: "))
 eps = 0.001
 
@@ -27,13 +42,7 @@ number = abs(s.find('.') - len(s)) - 1
 
 n = int(input("Число итераций n: "))
 
-intervalA = float(input("Левая граница интервала: "))
-intervalB = float(input("Правая граница интервала: "))
 
-# Строим график функций для наглядности
-x = np.linspace(intervalA, intervalB, 1000)
-y1 = fun_with_param(a, b, c, d, x)
-y2 = fun(x)
 
 print("\n-----------------------------------------------------------------")
 print("Метод простых итераций")
@@ -67,3 +76,13 @@ dichotomy_method_without_param(eps, number, intervalA, intervalB)
 
 # Показываем графики для метода дихотомии
 create_plot(x, y1, y2, "Метод дихотомии")
+
+print("-----------------------------------------------------------------")
+print("Метод хорд")
+
+# Ищем приближение для функции с параметрами
+hord_method_with_params(x0, n, a, b, c, d, eps, number)
+# Ищем приближение для функции без параметрами
+hord_method_without_params(x0, n, eps, number)
+# Показываем графики для метода дихотомии
+create_plot(x, y1, y2, "Метод хорд")
